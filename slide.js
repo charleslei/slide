c66f498a7778534909daa60f8821fd7b3617489f
chunlei.zhang <chunlei.zhang@qunar.com>
2014-04-15 14:31:10 +0800

    change params

--- slide.js
+++ slide.js
@@ -14,49 +14,49 @@
   }
   slide.prototype = {
     _init: function(){
+      var me = this;
       var component = {
         slideCtn: '.slide',
         bigLists: '.slide .big > .item',
         bigListsCtn: '.slide .big',
         smlLists: '.sml > .item',
         smlListsCtn: '.sml',
-        left: '.left',
-        right: '.right'
+        left: '.ctl .left',
+        right: '.ctl .right'
       };
+      var cfgs = me.configs;
+      var target = cfgs.tar;
+      var doms = {};
+      doms.slideCtn = target.find(component.slideCtn);
+      doms.bigLists = target.find(component.bigLists);
+      doms.bigListsCtn = target.find(component.bigListsCtn);
+      doms.smlLists = target.find(component.smlLists);
+      doms.smlListsCtn = target.find(component.smlListsCtn);
+      doms.left = target.find(component.left);
+      doms.right = target.find(component.right);
+      me.doms = doms;
 
-      var me = this;
-      var target = me.configs.tar;
-      me.doms = {};
-      me.doms.slideCtn = target.find(component.slideCtn);
-      me.doms.bigLists = target.find(component.bigLists);
-      me.doms.bigListsCtn = target.find(component.bigListsCtn);
-      me.doms.smlLists = target.find(component.smlLists);
-      me.doms.smlListsCtn = target.find(component.smlListsCtn);
-      me.doms.left = $(component.left);
-      me.doms.right = $(component.right);
-
-      //获取长度
-      me.configs.length = me.doms.bigLists.length;
-      var w = me.doms.slideCtn.width();
-      var h = me.doms.slideCtn.height();
-      me.configs.shownRect = {width: w, height: h};
-
-      //注册事件
-      me._addEvent();
+      //获取数量
+      me.length = doms.bigLists.length;
+      var w = doms.slideCtn.width();
+      var h = doms.slideCtn.height();
+      me.shownRect = {width: w, height: h};
 	  
-      var effect = me.configs.effect;
-      var doms = me.doms;
+      var effect = cfgs.effect;
       if(effect === 'normal'){
-        me.doms.bigLists.filter(':gt(0)').hide();
+        doms.bigLists.filter(':gt(0)').hide();
       }else if(effect === 'rtl' || effect === 'ltr'){
-        doms.bigListsCtn.append($(me.doms.bigLists.get(0)).clone()).width(target.width() * (me.configs.length+1));
+        doms.bigListsCtn.append($(doms.bigLists.get(0)).clone()).width(target.width() * (me.length+1));
       }
 
       //设置默认显示索引
-      me.currentEleIdx = me.configs.index;
+      me.currentEleIdx = cfgs.index;
 
       //启动自动播放
       me._autoSwitch();
+
+      //注册事件
+      me._addEvent();
     },
 
     _addEvent: function(){
@@ -77,14 +77,14 @@
 
     _next: function(curIdx, flag){//true: go ahead; false: go back;
       var me = this;
-      var len = me.configs.length;
+      var len = me.length;
       var nIdx = flag > 0 ? ((curIdx + 1) >= len ? 0 : (curIdx + 1)) : (curIdx - 1 < 0 ? len -1 : curIdx - 1);
       return nIdx;
     },
 
     _go: function(dir){
       var me = this;
-      if(me.configs.isAnimal){return;}
+      if(me.isAnimal){return;}
       var cIdx = me.currentEleIdx;
       var nIdx = me._next(cIdx,dir);
       me._switch(cIdx, nIdx, dir);
@@ -95,15 +95,15 @@
       var me = this;
       var effect = me.configs.effect;
       var lists = me.doms.bigLists;
-      var len = me.configs.length;
+      var len = me.length;
       if(effect === 'normal'){
         $(lists.get(cur)).hide();
         $(lists.get(next)).show();
         me._autoSwitch();
       }else if(effect === 'rtl' || effect === 'ltr'){ //right to left;
         var idx = me.currentEleIdx;
-        var width = me.configs.shownRect.width;
-        me.configs.isAnimal = true;
+        var width = me.shownRect.width;
+        me.isAnimal = true;
         if(next == len - 1 && dir < 0){
           me.doms.bigListsCtn.css('left', -len * width + 'px');
         }else if(next == 0 && dir > 0){
@@ -113,7 +113,7 @@
         }
 
         me.doms.bigListsCtn.animate({left: - next * width + "px"}, function(p){
-          me.configs.isAnimal = false;
+          me.isAnimal = false;
           me._autoSwitch();
         });
       }        
